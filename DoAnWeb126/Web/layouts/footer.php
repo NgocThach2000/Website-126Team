8cf35f700418344f934a9de412b8b8407496d0d9
= <=>
2020-06-02 12:28:34 +0700

    New update 12:28 02-06-2020

---
+++ DoAnWeb126/Web/layouts/footer.php
@@ -0,0 +1,108 @@
+<div class="footer">  
+            <div class="fixwidth clearfix">
+                <div class="content_footer">
+                    <div class="content_foot">
+                        <b>VỀ SPORTER.VN</b>
+                    </div>
+                    <p>Hệ thống bán lẻ đồ thể thao Sporter là đơn vị chuyên sản xuất và phân phối các sản phẩm thể thao chuyên nghiệp. Tại đây bạn có thể dễ dàng mua Quần áo và dụng cụ thể thao chất lượng cao, chính hãng... </p>
+                    <p><a class="Hotline">HOTLINE:19001000</a></p>
+                    <div class="img_icon">
+                        <a href="#"> <img class="icon1" src="<?php echo public_frontend() ?>img/icons8-facebook-100.png"></a>
+                        <a href="#"> <img class="icon2" src="<?php echo public_frontend() ?>img/icons8-instagram-50.png"></a>
+                        <a href="#"> <img class="icon3" src="<?php echo public_frontend() ?>img/icons8-twitter-48.png"></a>
+                
+                    </div>
+                </div>    <!-- -->
+            
+                <div class="content_footer2">  <!-- Footer2 -->
+                    <div class="content_foot2">
+                        <b>ĐĂNG KÝ NHẬN KHUYẾN MÃI</b>
+                    </div>
+                    <p>Hãy nhập email của bạn để chúng tôi gửi email ngay khi có thông tin về những chương trình khuyến mãi mới.</p>
+                    <form method="" action="" class="form_search2">                
+                        <input type="text" id="fname" name="fname"  placeholder="Nhập email của bạn"><br>
+                        <button class="button5">Đăng ký</button>
+                    </form>
+                </div>   
+
+                <div class="footer_right">
+                    <div class="boxfooter_fanpage">
+                        <div class="chat_facebook">
+                            <div class="fb-page fb_iframe_widget" data-href=" https://www.facebook.com/BeoBeoRestaurant/" data-tabs="timeline" data-width="500" data-height="230" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" fb-xfbml-state="rendered" fb-iframe-plugin-query="adapt_container_width=true&amp;app_id=&amp;container_width=423&amp;height=230&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2FBeoBeoRestaurant%2F&amp;locale=vi_VN&amp;sdk=joey&amp;show_facepile=true&amp;small_header=true&amp;tabs=timeline&amp;width=500"><span style="vertical-align: bottom; width: 423px; height: 230px;"><iframe name="f2ba25d82703e8c" width="500px" height="230px" data-testid="fb:page Facebook Social Plugin" title="fb:page Facebook Social Plugin" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" allow="encrypted-media" src="https://www.facebook.com/v2.10/plugins/page.php?adapt_container_width=true&amp;app_id=&amp;channel=https%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D46%23cb%3Df3456ddd5625608%26domain%3Dbeoquan.vn%26origin%3Dhttp%253A%252F%252Fbeoquan.vn%252Ff1ecc8a51423df8%26relation%3Dparent.parent&amp;container_width=423&amp;height=230&amp;hide_cover=false&amp;href=https%3A%2F%2Fwww.facebook.com%2FBeoBeoRestaurant%2F&amp;locale=vi_VN&amp;sdk=joey&amp;show_facepile=true&amp;small_header=true&amp;tabs=timeline&amp;width=500" style="border: none; visibility: visible; width: 423px; height: 230px;" class=""></iframe></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer2">
+            <div class="content_footer3 ">
+                <ul class="content_foot3 clearfix">
+                    <li><a href="#"> <img class="icon4" src="<?php echo public_frontend() ?>img/icons8-location-48.png"  width="18" > </a></li>
+                    <li>VietNam</li>
+                    <li class="copyright">©2020 Nike, Inc. All Rights Reserved</li>
+                </ul> 
+            </div>              
+        </div>   
+    </div>    
+
+    <div class="hotline-phone-ring-wrap">
+        <div class="hotline-phone-ring">
+            <div class="hotline-phone-ring-circle"></div>
+            <div class="hotline-phone-ring-circle-fill"></div>
+            <div class="hotline-phone-ring-img-circle">
+            <a href="tel:0987654321" class="pps-btn-img">
+                <img src="https://nguyenhung.net/wp-content/uploads/2019/05/icon-call-nh.png" alt="Gọi điện thoại" width="50">
+            </a>
+            </div>
+        </div>
+        <div class="hotline-bar">
+            <a href="tel:0987654321">
+                <span class="text-hotline">1900.1000</span>
+            </a>
+        </div>
+    </div>
+    <div class="mappage">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d501707.73905572796!2d106.40345095850728!3d10.765916392492459!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752eefdb25d923%3A0x4bcf54ddca2b7214!2sHo%20Chi%20Minh%20City%2C%20Vietnam!5e0!3m2!1sen!2s!4v1588424894071!5m2!1sen!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div> 
+    
+    <div class="loadmenu">
+        <nav id="my-menu">
+            <ul id="">
+                <li class=""><a href="#" id="Ao">Áo</a>
+                    <ul class="">
+                        <li><a href="#">Áo Nike</a></li>
+                        <li><a href="#">Áo Adidas</a></li>
+                        <li><a href="#">Áo Thái</a></li>
+                        <li><a href="#">Áo bóng đá</a></li>
+                    </ul>
+                </li>
+                <li class=""><a href="#">Quần</a>
+                    <ul class="">
+                        <li><a href="#">Quần Nike</a></li>
+                        <li><a href="#">Quần Adidas</a></li>
+                        <li><a href="#">Quần Thái</a></li>
+                        <li><a href="#">Quần bóng đá</a></li>
+                    </ul>
+                </li>
+                <li class=""><a href="#"><b>Giày</b></a>
+                    <ul class="">
+                        <li><a href="#">Giày thể thao</a></li>
+                        <li><a href="#">Giày bóng đá</a></li>
+                    </ul>
+                </li>
+                <li class=""><a href="#">Dụng cụ hỗ trợ</a>
+                    <ul class="">
+                        <li><a href="#">Túi thể thao</a></li>
+                        <li><a href="#">Băng tay</a></li>
+                        <li><a href="#">Băng gối</a></li>
+                        <li><a href="#">Băng chân</a></li>
+                    </ul>
+                </li>
+                <li><a href="#"><b>Sale</b></a></li>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>
