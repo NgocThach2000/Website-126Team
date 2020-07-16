d1141bc7a9c736a44d81b343669b8671bd5eb511
nguyenquangtruong <truongnguyenquang1611@gmail.com>
2020-07-16 09:34:00 +0700

    update shoppingcart

--- DoAnWeb126/Web/Shoping_cart1.php
+++ DoAnWeb126/Web/Shoping_cart1.php
@@ -1,6 +1,7 @@
 <?php 
     require_once __DIR__. "/autoload/autoload.php"; 
     $sum = 0;
+    $sum1=0;
     if(!isset($_SESSION['user_id'])){
         echo "<script>alert('Bạn chưa đăng nhập');location.href='Home.php'</script>";
     }
@@ -58,13 +59,33 @@
                                     <a href="#" class="btn btn-info updatecart" data-key=<?php echo $key ?>><i class="fa fa-refresh"></i>Cập nhật</a>
                                 </td>
                                 <?php $sum += $value['price']*$value['qty'];$_SESSION['tongtien']=$sum; ?> 
+                                <?php $sum1 +=$value['qty'];$_SESSION['a']=$sum1; ?> 
                                 
                             </tr>
                            
                     <?php $stt ++; endforeach ?>
-                        <tr>
-                            <td class="totalprice">Tổng tiền: <?php echo formatPrice($_SESSION['tongtien'])  ?>đ</td>          
-                        </tr>
+                    <tr>
+                        <td class='totalprice'>
+                                <?php
+                                if ($sum1<1){
+                                    echo 'Chưa có sản phẩm';
+                                }
+                                else {  
+                                    echo 'Tổng tiền: ';
+                                    echo  formatPrice($_SESSION['tongtien']) ;
+                                    echo 'đ';
+                                }
+                                ?>
+                        </td>
+                    <tr>
+                    <tr>
+                        <td class='totalprice'>
+                        <a href="List_Category.php?id=17" class="btn btn-danger"><?php if($sum1>=1){echo'Tiếp tục mua hàng';} else{ echo'Bắt đầu mua sắm';}?></a>
+                            <!--<input type="button" value='?php if($sum1>=1){echo'Tiếp tục mua hàng';} else{ echo'Bắt đầu mua sắm';}?>' class='btn btn-info updatecart'/>-->
+                            <!--<input type="button" value='?php if($sum1>=1){echo'Thanh toán';} else{ echo'Đăng xuất';}?>'class='btn btn-danger'/>-->
+                            <a href="<?php if($sum1>=1){echo'#';} else{ echo'Logout.php';}?>" class="btn btn-info"><?php if($sum1>=1){echo'Thanh toán';} else{ echo'Đăng xuất';}?></a>
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         </section>
